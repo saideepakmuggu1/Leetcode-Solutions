dd42adf32f14dc0e0aac911bdf992cfac530edf0
saideepakmuggu1 <150117874+saideepakmuggu1@users.noreply.github.com>
2024-07-08 07:37:44 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (85.35%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int[] v = new int[n+1];
-        Arrays.fill(v, -1);
-        for(int i = 0; i < nums.length; i++) {
-            v[nums[i]] = nums[i];
+        int n=nums.length;
+        int[] hash=new int[n+1];
+        for(int i=0;i<n;i++)
+        {
+            hash[nums[i]]++;
         }
-        for(int i = 0; i < v.length; i++) {
-            if(v[i] == -1) return i;
+        for(int i=0;i<=n;i++)
+        {
+            if(hash[i]==0)
+                return i;
         }
-        return 0;
+        return n;
     }
 }
